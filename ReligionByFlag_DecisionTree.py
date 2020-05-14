9ccda733d851d92d8a479c8d317b5dda1b1f784b
gurpreeth956 <gurpreeth956@gmail.com>
2020-05-14 18:01:39 -0400

    Cleaned up code to match new csv data

--- ReligionByFlag_DecisionTree.py
+++ ReligionByFlag_DecisionTree.py
@@ -5,24 +5,21 @@ from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("flags_data.csv")
 
-#Drop more data later, tree is too large
-X = data.drop(['religion', 'topleft', 'botright', 'name', 'mainhue'], axis=1)
+# Drop more data later, tree is too large
+x = data.drop(['religion', 'topleft', 'botright', 'name', 'mainhue'], axis=1)
 y = data['religion']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
-
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30)
 classifier = tree.DecisionTreeClassifier()
-classifier.fit(X_train, y_train)
-
-y_pred = classifier.predict(X_test)
-
+classifier.fit(x_train, y_train)
+y_pred = classifier.predict(x_test)
 feature_arr = ['landmass','zone','area','population','language','bars','stripes','colors','red','green','blue','gold','white','black','orange','circles','crosses','saltires','quarters','sunstars','crescent','triangle','icon','animate','text']
-class_arr = ['Catholic', 'Other Christian', 'Muslim', 'Buddhist', 'Hindu', 'Ethnic', 'Marxist', 'Others']
+class_arr = ['Christian', 'Muslim', 'Other']
 
-fig, ax = plt.subplots(figsize=(20,20))
+fig, ax = plt.subplots(figsize=(20, 20))
 
-#Tree is too large to display properly, change max depth, fontsize and DPI to adjust info/size
-tree.plot_tree(classifier, feature_names= feature_arr, class_names= class_arr, ax=ax, filled=True, fontsize = 5) #max_depth=4
-#save tree as png if you want to zoom in
+# Tree is too large to display properly, change max depth, fontsize and DPI to adjust info/size
+tree.plot_tree(classifier, feature_names=feature_arr, class_names=class_arr, ax=ax, filled=True, fontsize=5, max_depth=4) #max_depth=4
+# Save tree as png if you want to zoom in
 plt.savefig('flag_tree', dpi=100)
+plt.tight_layout()
 plt.show()
--- ReligionByFlag_KNNClassifier.py
+++ ReligionByFlag_KNNClassifier.py
@@ -2,28 +2,28 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.colors import ListedColormap
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import StandardScaler
-from matplotlib.colors import ListedColormap
-import matplotlib.patches as patches
+from sklearn.metrics import classification_report
 
 
 # Import data from csv file
 data = pd.read_csv('flags_data.csv', delimiter=',')
 
 # Pick the important attributes
-x = data.iloc[:, [3, 4]].values
+x = data.iloc[:, [1, 5]].values
 y = data.iloc[:, 29].values
 
 # Split into testing and training set
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.3)
 
 # Normalize data before making predictions
-normalize_scale = StandardScaler()
-normalize_scale.fit(x_train)
-x_train = normalize_scale.transform(x_train)
-x_test = normalize_scale.transform(x_test)
+# normalize_scale = StandardScaler()
+# normalize_scale.fit(x_train)
+# x_train = normalize_scale.transform(x_train)
+# x_test = normalize_scale.transform(x_test)
 
 # Create x and y ranges for the plot
 x_min = x_train[:, 0].min() - 1
@@ -33,8 +33,7 @@ y_max = x_train[:, 1].max() + 1
 x_grid, y_grid = np.meshgrid(np.arange(x_min, x_max, .01), np.arange(y_min, y_max, .01))
 
 # Use KNN classifier to predict religion
-neighbors = 5
-kNN_class = KNeighborsClassifier(neighbors, weights='distance')
+kNN_class = KNeighborsClassifier(n_neighbors=5, weights='distance')
 kNN_class.fit(x_train, y_train)
 z = kNN_class.predict(np.c_[x_grid.ravel(), y_grid.ravel()])
 z_grid = z.reshape(x_grid.shape)
@@ -44,12 +43,12 @@ plt.figure()
 light_Colors = ListedColormap(['#F39D9D', '#93F088', '#67AAF1'])
 dark_Colors = ListedColormap(['#F32C2C', '#219912', '#0E67C5'])
 plt.pcolormesh(x_grid, y_grid, z_grid, cmap=light_Colors)
-plt.scatter(x_train[:, 0], x_train[:, 1], c=y_train, cmap=dark_Colors)
+plt.scatter(x_train[:, 0], x_train[:, 1], cmap=dark_Colors, c=y_train)
 plt.xlim(x_grid.min(), x_grid.max())
 plt.ylim(y_grid.min(), y_grid.max())
 plt.title('Religion 5-NN Classifier')
-plt.xlabel('area')
-plt.ylabel('population')
+plt.xlabel('landmass')
+plt.ylabel('language')
 red_patch = patches.Patch(color='red', label='Christian')
 green_patch = patches.Patch(color='green', label='Islam')
 blue_patch = patches.Patch(color='blue', label='Other')
@@ -57,3 +56,21 @@ plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), handles=[red_patch, green
 plt.tight_layout()
 plt.show()
 
+y_prediction = kNN_class.predict(x_test)
+print(classification_report(y_test, y_prediction))
+
+# Calculate error for KNN from 1 to 25
+errors = []
+for i in range(1, 25):
+    knn = KNeighborsClassifier(n_neighbors=i, weights='distance')
+    knn.fit(x_train, y_train)
+    y_prediction = knn.predict(x_test)
+    errors.append(np.mean(y_prediction != y_test))
+
+# Plot KNN Classifier error
+plt.figure()
+plt.plot(range(1, 25), errors, color='red', marker='o', markersize=8)
+plt.title('K-Value Error Rate')
+plt.xlabel('K-Value')
+plt.ylabel('Error')
+plt.show()
